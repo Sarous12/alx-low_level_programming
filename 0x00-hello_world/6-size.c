edd4f3f3403028d52d88437d911178b24010aa2e
Sarous12 <adewalekaocharayabo@gmail.com>
2022-03-14 21:36:42 +0000

    My task 6 commit message

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*
+*main - Entry point
+*
+*Return always 0 (Sucess)
+*/
+int main(void)
+[
+printf("Size of char: 3d byte(s)\n". Sizeof(char));
+printf("Size of an int: %d bytes(s)\n. sizeof(int));
+printf("Size of a long int %d bytes (s)\n".sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n". sizeof(long long int));
+printf("Size of a float: %d byte(s)\n". sizeof(float));
+return (0);
+]
