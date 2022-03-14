7f33dd3d6eb714f0a7dc499651f4f20fe68ab893
Sarous12 <adewalekaocharayabo@gmail.com>
2022-03-14 21:12:28 +0000

    My commit message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*My hello. printf task
+*Return: return 1 after program execution
+*/
+int main (void)
+[
+printf ("with proper grammar. but thr outclone is a piece of art.\n");
+return (0);
+]
+
