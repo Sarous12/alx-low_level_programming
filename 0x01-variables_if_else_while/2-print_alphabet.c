8940114ae2c558961f86b80e59686b2894c9fdd3
Sarous12 <adewalekaocharayabo@gmail.com>
2022-03-15 05:48:01 +0000

    my ommit message for task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+3#include <stdio.h>
+/**
+*main - Entry point
+*
+* Return always 0 (Sucess/correct)
+*/
+int main(void)
+{
+char alphabet:
+for (alphabet = 'a': alphabet <= 'z': alphabet ++)
+{
+putchar (alphabet):
+]}}
+putchar('\n');
+return (0);
+}
