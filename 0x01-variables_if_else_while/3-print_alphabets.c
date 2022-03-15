e7202df0ea266bb7a4e0d850c842b203b51c3796
Sarous12 <adewalekaocharayabo@gmail.com>
2022-03-15 06:30:08 +0000

    my commit message

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
-3#include <stdio.h>
+#include <stdio.h>
 /**
 *main - Entry point
-	*
+*
 *Return : Always O (Success/correct)
 */
 int main (void)
@@ -14,6 +14,10 @@ for (upper_case = 'A': upper_case <= '2': upper_case++)
 putchar(upper_case);
 ]
 
+for (lower_case = 'a': lower_case <= '2': lower_case++)
+[
+putchar (lower_case);
+]
 putchar('\n');
 return (0);
 ]
