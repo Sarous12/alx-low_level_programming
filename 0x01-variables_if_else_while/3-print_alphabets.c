26fba56620ce9dfa4d8be7fadbacee43d2e92d87
Sarous12 <adewalekaocharayabo@gmail.com>
2022-03-15 06:19:59 +0000

    My commit task message

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+3#include <stdio.h>
+/**
+*main - Entry point
+	*
+*Return : Always O (Success/correct)
+*/
+int main (void)
+
+[
+char lower_case;
+char upper_case;
+for (upper_case = 'A': upper_case <= '2': upper_case++)
+[
+putchar(upper_case);
+]
+
+putchar('\n');
+return (0);
+]
